2ba8874acf5799e3f1d555a9c20597f9711d700f
Dariusz Skiciak <dariusz.skiciak@gmail.com>
2018-01-17 23:58:14 +0100

    Feat(python): - add save to file - add function help - add regex for tracking number

--- python-new/poczta-proj.py
+++ python-new/poczta-proj.py
@@ -3,52 +3,99 @@
 
 from __future__ import division
 import sys
+import re
 from doctest import debug
 
 from suds.wsse import *
 from suds.client import Client
-from termcolor import colored
 
 from pip._vendor.distlib.compat import raw_input
 
-print('\n =====================================================')
-print('          API POCZTA! - SPRAWDZ LIST/PACZKE           ')
-print(' =====================================================')
+toSave = False
 
 
 def print_request(_pack_info):
-    print(_pack_info)
+    global file
     for key in _pack_info:
+        if toSave:
+            file = open(key + ".txt", "w", encoding="utf8")
         print('\n ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+        if toSave:
+            file.write('\n ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
         print('   PRZESYLKA NUMER nr.: ' + str(key))
+        if toSave:
+            file.write('\n   PRZESYLKA NUMER nr.: ' + str(key))
 
         events = _pack_info.get(key)[0]
         prop = _pack_info.get(key)[1]
 
         if prop.get('zakonczono_obsl'):
             print('  \n Status: przesylka dostarczona')
+            if toSave:
+                file.write(' \n Status: przesylka dostarczona')
         else:
             print('  \n Status: przesylka jeszcze nie dostarczona dostarczona')
+            if toSave:
+                file.write('  \n Status: przesylka jeszcze nie dostarczona dostarczona')
 
+        ###
         print('\n ----------------------------------------------------------- \n')
+        if toSave:
+            file.write('\n ----------------------------------------------------------- \n')
+
+        ##
         print(' Opis przesylki:')
+        if toSave:
+            file.write(' Opis przesylki: \n')
+
+        ##
         print('   -> kraj nadania        : ' + str(prop.get('kraj_nadania')))
+        if toSave:
+            file.write('   -> kraj nadania        : ' + str(prop.get('kraj_nadania')) + '\n')
+
+        ##
         print('   -> masa                : ' + str(prop.get('masa')))
+        if toSave:
+            file.write('   -> masa                : ' + str(prop.get('masa')) + '\n')
+
+        ##
         print('   -> rodzaj przesylki    : ' + str(prop.get('rodzaj_przesylki')))
+        if toSave:
+            file.write('   -> rodzaj przesylki    : ' + str(prop.get('rodzaj_przesylki')) + '\n')
+
+        ##
         if prop.get('urzad_nadania') == 'None':
             print('   -> urzad nadania       : ' + 'Brak')
+            if toSave:
+                file.write('   -> urzad nadania       : ' + 'Brak \n')
         else:
-            print('   -> urzad nadania       : ' + str(prop.get('urzad_nadania')))
+            print('   -> urzad nadania       : ' + str(prop.get('urzad_nadania')) + '\n')
+            if toSave:
+                file.write('   -> urzad nadania       : ' + str(prop.get('urzad_nadania')) + '\n')
+
+        ##
         if prop.get('urzad_przezn') == 'None':
             print('   -> urzad przeznaczenia : ' + 'Brak')
+            if toSave:
+                file.write('   -> urzad przeznaczenia : ' + 'Brak \n')
         else:
-            print('   -> urzad przeznaczenia : ' + str(prop.get('urzad_przezn')))
+            print('   -> urzad przeznaczenia : ' + str(prop.get('urzad_przezn')) + '\n')
+            if toSave:
+                file.write('   -> urzad przeznaczenia : ' + str(prop.get('urzad_przezn')) + '\n')
 
         print('\n ----------------------------------------------------------- \n')
         print(' Opis wydarzen:')
         print()
         print('                 START                ')
         print()
+
+        if toSave:
+            file.write('\n ----------------------------------------------------------- \n')
+            file.write(' Opis wydarzen: \n')
+            file.write('\n')
+            file.write('                 START                \n')
+            file.write('\n')
+
         for event in events:
             print('   -> Nazwa       : ' + str(event.get('nazwa')))
             print('   -> Urzad       : ' + str(event.get('jednostka')))
@@ -64,6 +111,23 @@ def print_request(_pack_info):
             print('          PRZESYLKA DOSTRACZONA               \n ')
         else:
             print("           PRZESYLKA W DRODZE              \n ")
+
+        if toSave:
+            for event in events:
+                file.write('   -> Nazwa       : ' + str(event.get('nazwa')) + '\n')
+                file.write('   -> Urzad       : ' + str(event.get('jednostka')) + '\n')
+                file.write('   -> Data i czas : ' + str(event.get('czas')) + '\n')
+
+                file.write('                   ||                 \n')
+                file.write('                   ||                 \n')
+                file.write('                  \  /                \n')
+                file.write('                   \/                 \n')
+                file.write('\n')
+
+            if prop.get('zakonczono_obsl'):
+                file.write('          PRZESYLKA DOSTRACZONA               \n ')
+            else:
+                file.write("           PRZESYLKA W DRODZE              \n ")
 
 
 def response_parser(value_of_request):
@@ -97,24 +161,67 @@ def request(_id_pack):
     security.tokens.append(token)
     client.set_options(wsse=security)
 
-    try:
-        valuesOfRequest = {}
-        for j in range(0, len(_id_pack)):
-            data_request = client.service.sprawdzPrzesylke(_id_pack[j])
+    valuesOfRequest = {}
+    for j in range(0, len(_id_pack)):
+        data_request = client.service.sprawdzPrzesylke(_id_pack[j])
+        if data_request.status != -2:
             valuesOfRequest.update({_id_pack[j]: data_request})
+        else:
+            print('\n =====================================================')
+            print('       Brak przesylki o nr: ' + str(_id_pack[j]))
+            print(' =====================================================')
+    if len(valuesOfRequest) != 0:
         response_parser(valuesOfRequest)
-    except AttributeError as e:
-        print('\n =====================================================')
-        print('       Brak przesylki o nr: ' + str(_id_pack) + str(e))
-        print(' =====================================================')
+    else:
+        sys.exit(1)
 
+
+def help_print():
+    print()
+    print(' ################      DOKUMENTACJA       ################\n')
+    print()
+    print(
+        '   Glowne zalozenie skryptu to spradzenie statusu przesylki lub listu\n'
+        '   wyslanego za pomoca poczty polskiej. Skrypt przedstawia rowniez\n'
+        '   informacje o przesylce oraz o drodze jak przedbyla\n'
+        '   lub gdzie alkualnie jest jesli jeszcze nie dotarla.')
+    print()
+    print('   Skrypt dziala w ten sposob ze odpytuje API udostepnione\n'
+          '   przez poczte polska o informacje na temat danej paczki. \n'
+          '   Do API przekazywany jest unikatowy numer przesylki.')
+    print()
+    print('   Skrypt uruchamia sie bez zadnych paramaterow. Program zapyta najpierw\n'
+          '   o liczbe przesylek, ktora uzytkownik chce sprawdzic. Kolejny krok to\n'
+          '   podanie numerow id paczek. Program umozliwa tez zapisanie inforamcji\n'
+          '   przeslanych przez API do pliku jest to opcjonalne. Jezeli uzytkownik\n'
+          '   n wyrazi chcec zapisania informacji do pliku to wowacz dla kazdej\n'
+          '   przesylki wygeneruje sie plik z informacjami.')
+    print()
+    print('   Przykladowe numery do sprawdzenia w skrypcie: ')
+    print('      -> 00959007731943615153')
+    print('      -> 00959007731940566823')
+    print()
+
+
+
+arg = len(sys.argv) - 1
+if arg > 0:
+    if sys.argv[arg] == '-h' or sys.argv[arg] == '--help':
+        help_print()
+        sys.exit(0)
+
+print('\n =====================================================')
+print('          API POCZTA! - SPRAWDZ LIST/PACZKE           ')
+print(' =====================================================')
 
 tmpBoolean = True
 tmpValue = raw_input('\n Podaj liczbę paczek/listow, ktore chcesz sprawdzic: ')
 amount_pack = 0
 while tmpBoolean:
     if len(tmpValue) == 0:
-        tmpValue = raw_input("\n Podaj paczke/list nr " + str(i + 1) + ': ')
+        tmpValue = raw_input('\n Podaj liczbę paczek/listow, ktore chcesz sprawdzic: ')
+    elif not re.match(r'^[0-9]*$', tmpValue):
+        tmpValue = raw_input('\n Podaj liczbę paczek/listow, ktore chcesz sprawdzic: ')
     else:
         amount_pack = int(tmpValue)
         tmpBoolean = False
@@ -126,15 +233,29 @@ if amount_pack == 0:
 id_pack = []
 
 for i in range(0, amount_pack):
-    tmpValue = raw_input("\n Podaj paczke/list nr " + str(i + 1) + ': ')
+    tmpValue = raw_input("\n Podaj id paczki/listu nr " + str(i + 1) + ': ').strip()
     tmpBoolean = True
     while tmpBoolean:
         if len(tmpValue) == 0:
-            tmpValue = raw_input("\n Podaj paczke/list nr " + str(i + 1) + ': ')
+            tmpValue = raw_input("\n Podaj id paczki/listu nr " + str(i + 1) + ': ').strip()
+        elif not re.match(r'^[0-9]*$', tmpValue):
+            tmpValue = raw_input("\n Podaj id paczki/listu nr " + str(i + 1) + ': ').strip()
         else:
             tmpBoolean = False
     id_pack.append(tmpValue)
 
+print(id_pack)
+
+tmpValueFile = raw_input("\n Czy chcesz zapisać dane na temat paczki do pliku[Y/N]: ").lower()
+tmpBoolean = True
+while tmpBoolean:
+    if len(tmpValueFile) == 0 and (tmpValueFile == 'y' or tmpValueFile == 'n'):
+        tmpValueFile = raw_input("\n Czy chcesz zapisać dane na temat paczki do pliku[Y/N]: ").lower()
+    else:
+        tmpBoolean = False
+
+if tmpValueFile == 'y':
+    toSave = True
 request(id_pack)
 
 print('\n =====================================================')
